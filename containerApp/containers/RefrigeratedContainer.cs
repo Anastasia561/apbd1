50ec98c32f89d198148920e32cb867d3dd455103
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2025-03-17 19:28:03 +0100

    refrigerated container implemented

---
+++ containerApp/containers/RefrigeratedContainer.cs
@@ -0,0 +1,45 @@
+﻿namespace containerApp;
+
+public class RefrigeratedContainer : Container
+{
+    public string ProductType { get; set; }
+    public double Temperature { get; set; }
+
+    private Dictionary<string, double> _productsToTemperatures = new Dictionary<string, double>
+    {
+        { "Bananas", 13.3 },
+        { "Chocolate", 18 },
+        { "Fish", 2 },
+        { "Meat", -15 },
+        { "Ice cream", -18 },
+        { "Frozen pizza", -30 },
+        { "Cheese", 7.2 },
+        { "Sausages", 5 },
+        { "Butter", 20.5 },
+        { "Eggs", 19 }
+    };
+
+    public RefrigeratedContainer(int height, int depth, int weight, int maxPayload,
+        double temperature, string productType)
+        : base(height, depth, weight, maxPayload)
+    {
+        ProductType = productType;
+        if (temperature < _productsToTemperatures[ProductType])
+        {
+            Console.WriteLine($"Temperature for {ProductType} should be greater than" +
+                              $" {_productsToTemperatures[ProductType]}");
+            Temperature = _productsToTemperatures[ProductType];
+        }
+        else
+        {
+            Temperature = temperature;
+        }
+
+        SerialNumber = "KON-R-" + GenerateSerialNumber();
+    }
+
+    public override void EmptyCargo()
+    {
+        Console.WriteLine($"Cargo emptied ({ProductType})");
+    }
+}
